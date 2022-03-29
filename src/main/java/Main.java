5164eca34e64ea65aa70116b51d38b3c34aca82e
elodin19 <joanoldaniel@gmail.com>
2022-03-29 10:27:01 +0200

    it's working. need to refactor it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,21 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> y = Arrays.asList( new Integer[] {3, 3, 4, 7, 6, 4, 1, 4, 10, 5, 3, 9, 8, 9});
-        List<Integer> x = Arrays.asList( new Integer[] {
-
-                6,
-                3,
-                1, 2, 3,
-                3,
-                3, 3, 3,
-                3,
-                2, 2, 3,
-                4,
-                10, 2, 4, 4,
-                5,
-                10, 10, 10, 5, 10,
-                6,
-                1, 2, 3, 4, 5, 6});
+        List<Integer> x = Arrays.asList( new Integer[] {6, 3, 1, 2, 3, 3, 3, 3, 3, 3, 2, 2, 3, 4, 10, 2, 4, 4, 5, 10, 10, 10, 5, 10, 6, 1, 2, 3, 4, 5, 6});
 
         List<Double> result = solve(x);
 
@@ -30,27 +16,52 @@ public class Main {
             System.out.println(n);
     }
 
-    public static List<Double> solve(List<Integer> y){
+    public static List<Double> solve(List<Integer> originalList){
 
         final DecimalFormat df = new DecimalFormat("#.##");
         List<Double> result = new ArrayList<>();
 
         //Starts in 1 cause the position 0 has the value of the size of list with the response,
         //  but we don't need it  in Java
-        for (int i = 1; i < y.size(); i++){
+        for (int i = 1; i < originalList.size(); i++){
 
             double sum = 0.00;
+            int newValue = 0;
+            List<Integer> newArray = new ArrayList<>();
 
-            // i will have the lenght of the next array
-            // j will run through i + 1 + the value in i
+            // i will have the length of the next array
+            // j and m will run through i + 1 + the value in i
             //   In case we have i = 1 and the value in the index 1 is 3, we will read the indexes from 2 to 4
-            for (int j = i + 1; j <= i + y.get(i); j++){
+            // In this loop we create an array with the new values
+            for (int m = i + 1; m <= i + originalList.get(i); m++){
 
-                //TODO: debug
-                sum += (double) (y.get(i)+1) / (1 + y.get(j));
+                for (int k = i + 1; k <= i + originalList.get(i); k++){
+
+                    if (originalList.get(m) <= originalList.get(k)) newValue ++;
+                }
+
+                newArray.add(newValue);
+                newValue = 0;
             }
 
-            i += y.get(i);
+            for (int j = i + 1, m = 0; j <= i + originalList.get(i); j++, m++){
+
+                //sum += (double) (originalList.get(i)+1) / (1 + originalList.get(j));
+
+                /*if (newArray.get(m) == 1){
+                    sum++;
+                } else {
+                    sum += (double) (originalList.get(i)+1) / newArray.get(m) + 1 ;
+                }*/
+
+                sum += (double) (originalList.get(i)+1) / (newArray.get(m) + 1);
+
+                System.out.println((originalList.get(i)+1) + " --- " + newArray.get(m));
+            }
+
+            System.out.println("Fin de for. Sum = " + sum);
+
+            i += originalList.get(i);
 
             //Saves with only 2 decimals houses
             result.add( Math.floor(sum * 100) / 100);
